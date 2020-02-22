e8ad4e58052333ce7716c3c9a1c485e1e29a54ba
Alessia <miccoli.alessia@hotmail.it>
2020-02-22 11:36:36 +0100

    added credits

--- fe/fantawiz-fe/src/App.vue
+++ fe/fantawiz-fe/src/App.vue
@@ -20,6 +20,24 @@
       <Search :headers="headers" :search="search" @close="resetReserch"/>
       <Favourites :headers="headers"/>
     </v-content>
+    <v-footer color="primary" id="footer">
+      <div id="credits">
+        <p> Fonti: </p>
+        <a target="_blank" href="https://www.gazzetta.it/">
+          <v-img src="../src/assets/icons/logos/gazzetta.jpg" width="30px"></v-img>
+        </a>
+        <a target="_blank" href="https://www.fantacalcio.it/">
+          <v-img src="../src/assets/icons/logos/fanta-gazzetta.png" width="30px"></v-img>
+        </a>
+        <a target="_blank" href="https://sport.sky.it/">
+          <v-img src="../src/assets/icons/logos/sky-sport.png" width="30px"></v-img>
+        </a>
+        <a target="_blank" href="https://www.corrieredellosport.it/">
+          <v-img src="../src/assets/icons/logos/corriere-dello-sport.png" width="30px"></v-img>
+        </a>
+      </div>
+      <div>Icons made by <a target="_blank" href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a target="_blank" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </v-footer>
   </v-app>
 </template>
 
@@ -58,3 +76,53 @@ export default {
   }
 };
 </script>
+
+<style>
+
+#credits{
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
+ #footer{
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   flex-direction: column;
+   font-size: small;
+   color: white;
+ }
+
+a{
+  cursor: pointer;
+  color: lightgray !important;
+}
+
+#credits a{
+  margin-left: 10px;
+}
+
+a div{
+  opacity: 0.85;
+}
+
+a div:hover{
+  opacity: 1;
+}
+
+h2{
+  margin-bottom: 20px;
+}
+
+.v-application p{
+  margin-bottom: 0 !important;
+}
+
+.v-image{
+  border-radius: 50% !important;
+}
+
+</style>
