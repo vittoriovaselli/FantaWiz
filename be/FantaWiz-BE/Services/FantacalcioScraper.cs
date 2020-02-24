17fa0c37c9ead0b39fc18f7840a77ef482ec074e
Vittorio Vaselli <vittorio.vaselli@outlook.com>
2020-02-24 17:16:23 +0100

    scrape starting and reserves from fantacalcio

--- be/FantaWiz-BE/Services/FantacalcioScraper.cs
+++ be/FantaWiz-BE/Services/FantacalcioScraper.cs
@@ -20,7 +20,78 @@ namespace FantaWizBE.Services
 
             var teams = GetTeams();
 
+            GetStartingAndReserves(teams);
+
             return _players;
+        }
+
+        private void GetStartingAndReserves(string[] teams)
+        {
+            var groups = pageDocument
+                .DocumentNode
+                .SelectNodes("//div[contains(@class, 'col-lg-6 col-md-6 col-sm-6 col-xs-6 probbar p10')]")
+                .ToArray();
+
+            //get home teams starting players
+            var teamIndex = 0;
+            for(int i = 0; i< groups.Length; i+=4)
+            {
+                var playersArray = groups[i]
+                   .SelectNodes(".//div[contains(@class, 'pname2')]")
+                   .Select(x => x.InnerText)
+                   .ToArray();
+                foreach (var player in playersArray)
+                {
+                    AddPlayer(teams, teamIndex, player, PlayerStatus.Starting);
+                }
+                teamIndex+=2;
+            }
+
+            //get away teams starting players
+            teamIndex = 1;
+            for (int i = 1; i < groups.Length; i += 4)
+            {
+                var playersArray = groups[i]
+                   .SelectNodes(".//div[contains(@class, 'pname2')]")
+                   .Select(x => x.InnerText)
+                   .ToArray();
+                foreach (var player in playersArray)
+                {
+                    AddPlayer(teams, teamIndex, player, PlayerStatus.Starting);
+                }
+                teamIndex += 2;
+            }
+
+            //get home teams reserves
+            teamIndex = 0;
+            for (int i = 2; i < groups.Length; i += 4)
+            {
+                var playersArray = groups[i]
+                   .SelectNodes(".//div[contains(@class, 'pname2')]")
+                   .Select(x => x.InnerText)
+                   .ToArray();
+                foreach (var player in playersArray)
+                {
+                    AddPlayer(teams, teamIndex, player, PlayerStatus.Reserve);
+                }
+                teamIndex += 2;
+            }
+
+            //get away teams reserves
+            teamIndex = 1;
+            for (int i = 3; i < groups.Length; i += 4)
+            {
+                var playersArray = groups[i]
+                   .SelectNodes(".//div[contains(@class, 'pname2')]")
+                   .Select(x => x.InnerText)
+                   .ToArray();
+                foreach (var player in playersArray)
+                {
+                    AddPlayer(teams, teamIndex, player, PlayerStatus.Reserve);
+                }
+                teamIndex += 2;
+            }
+
         }
 
         private string[] GetTeams()
